79a7b879b9dc178e60828e778c6ce2ce49da8ffd
Gideon Dresdner <gideond@gmail.com>
2012-12-29 18:53:43 -0500

    has all basic options.

--- scripts/map_probes.py
+++ scripts/map_probes.py
@@ -20,7 +20,7 @@ import re
 import argparse
 
 def markers_hash(markerfile):
-    sys.stderr.write("hashing probes...\n")
+    sys.stderr.write("hashing probes...")
 
     lines = markerfile.readlines()
 
@@ -52,10 +52,10 @@ def markers_hash(markerfile):
 
         hash[probe_name] = [chr, pos]
 
-    sys.stderr.write("...DONE!  (skipped %d X/Y probes)\n" % skipped_XY)
+    sys.stderr.write("DONE!  (skipped %d X/Y probes)\n\n" % skipped_XY)
     return hash
 
-def map_snp_marker_data(hash, snp_lines):
+def map_snp_marker_data(hash, snp_lines, diagnostic):
     # takes a hash map like this {'CN_473964': [1, 61808]} and lines from a snp
     # markers file like this
         # header lines
@@ -64,18 +64,23 @@ def map_snp_marker_data(hash, snp_lines):
     #
     # header lines are assumed to contain the string "REF" and are ignored
     # based on this criteria
+    #
+    # diagnostic: boolean. Print things out in format for CBS or just do a diagnostic?
 
     out = sys.stdout
 
     REF = re.compile("REF")
 
+    unmapped = 0
+    first_unmapped = ''
+    line_no = 0
     for line in snp_lines:
+        line_no+=1
         if REF.search(line):
             continue
         line = line.strip()
-        line = line.split(" ")                          # data has no whitespace
+        line = line.split()                          # assume: data has no whitespace
         line = filter(lambda x: x != '', line)
-        #print line
 
         probe, signal = line[0], line[1]
         try:
@@ -84,33 +89,32 @@ def map_snp_marker_data(hash, snp_lines):
             chr = locus[0]
             pos = locus[1]
 
-            out.write("%s\t%d\t%d\t%s\n" \
-                    % (probe, hash[probe][0], hash[probe][1], signal))
+            if not diagnostic:
+                out.write("%s\t%d\t%d\t%s\n" \
+                        % (probe, chr, pos, signal))
         except KeyError:
             unmapped += 1
+            if first_unmapped == '':
+                first_unmapped = probe
+                first_unmapped_line = line_no
+    sys.stderr.write("\n%d unmapped probe(s)\n" % unmapped)
+    if first_unmapped != '':
+        sys.stderr.write("1st unmapped probe: %s (line %d) \n" % (first_unmapped, first_unmapped_line))
 
 ### arg parser ###
-
-def map_wrap(args):
-    # wrapper to take commandline arguments and run map_snp_marker_data on them
-    hash = markers_hash(args.marker_positions)
-
-    snp_lines = sys.stdin.readlines()
-    #print snp_lines
-
-    map_snp_marker_data(hash, snp_lines)
-
 parser = argparse.ArgumentParser(description="Utils for dealing with \
         sequencing probes.  Takes the probes in question via stdin")
 subparsers = parser.add_subparsers()
 
-#diagnostic = subparsers.add_parser('diagnostic', help="diagnostic of unmapped probes")
-#diagnostic.set_defaults(func=foo)
+diagnostic = subparsers.add_parser('diagnostic', help="diagnostic of unmapped probes")
+diagnostic.set_defaults(func=lambda args:
+        map_snp_marker_data(markers_hash(args.marker_positions), \
+            sys.stdin.readlines(), diagnostic=True))
 
 map = subparsers.add_parser('map', help="map away to standard out")
 map.set_defaults(func=lambda args:
         map_snp_marker_data(markers_hash(args.marker_positions), \
-            sys.stdin.readlines()))
+            sys.stdin.readlines(), diagnostic=False))
 
 parser.add_argument('marker_positions', help='file of marker positions, \
         e.g. lines like this SNP_A-1738457    1   328296 ', type=argparse.FileType('r'))
