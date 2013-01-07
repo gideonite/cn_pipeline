eb4f07614144a82e48e133c2c7165e0eccfcd5f3
Gideon Dresdner <gideond@gmail.com>
2013-01-07 12:12:28 -0500

    wrote nearest_probe. cleanup.

--- scripts/map_probes.py
+++ scripts/map_probes.py
@@ -18,6 +18,7 @@
 import sys
 import re
 import argparse
+from bisect import bisect_left, bisect_right
 
 REF = re.compile("REF")
 barcode = re.compile("barcode")
@@ -87,13 +88,14 @@ def read_cbs_segments(_in):
 class MarkerPosUtil:
     def __init__(self, markerfile):
         self.marker_f = markerfile
-        self.hash = self.make_hash(markerfile)
-        self.chr_pos_hash = None      # initialize
+        self.chrPos_name_hash = self.make_chrPos_name_hash(markerfile)
+        self.locus_hash = None      # initialize
 
-    # {{{ make_hash
-    def make_hash(self, markerfile):
-        # someday this may want to be implemented differently, i.e. with a
-        # "Marker class"
+    def make_chrPos_name_hash(self, markerfile):
+        # returns a two-way hash (chr, pos) <-> probe_name
+        #
+        # (someday this may want to be implemented differently, i.e. with a
+        # "Marker class")
         sys.stderr.write("hashing probes...")
 
         lines = markerfile.readlines()
@@ -130,10 +132,11 @@ class MarkerPosUtil:
 
         sys.stderr.write("DONE!  (skipped %d X/Y probes)\n\n" % skipped_XY)
         return hash
-    #}}}
 
-    def make_chr_pos_hash(self):
-        # initialize hash map of { chr : position }
+    def make_locus_hash(self):
+        # returns a hash of { chr : position } where position is the locus on the chr
+
+        # initialize the map
         hash = {}
         for i in range(1,24):
             if i == 23:
@@ -141,55 +144,105 @@ class MarkerPosUtil:
             else:
                 key = str(i)
                 hash[key] = []
+        # end
 
-        for key in self.hash.keys():
+        for key in self.chrPos_name_hash.keys():
             if type(key) == tuple:
                 hash[key[0]].append(key[1])
 
-        self.chr_pos_hash = hash
+        return hash
 
-    def map_marker_signals(self, marker_signals):
-        sys.stderr.write("mapping marker signals...")
+    def get_locus_hash(self):
+        if self.locus_hash == None:
+            self.locus_hash = self.make_locus_hash()
+        return self.locus_hash
+
+    def map_markers(self, markers):
+        # map a list of marker names using the hash table
+        # and report some stats
+        # marker name, e.g. CN_473982
+
+        sys.stderr.write("mapping markers...")
         unmapped = 0
         first_unmapped = ''
         line_no = 0
 
-        for ms in marker_signals:
+        for m in markers:
             line_no+=1
             try:
-                self.hash[ms.name]
+                self.chrPos_name_hash[m]
             except KeyError:
                 unmapped += 1
                 if first_unmapped == '':
-                    first_unmapped = ms
+                    first_unmapped = m
                     first_unmapped_line = line_no
 
         sys.stderr.write("DONE!\n")
 
         sys.stderr.write("\n%d unmapped probe(s)\n" % unmapped)
         if first_unmapped != '':
-            sys.stderr.write("1st unmapped probe: %s (line %d) \n" % (first_unmapped, first_unmapped_line))
+            sys.stderr.write("1st unmapped probe: %s (line %d) \n" % (first_unmapped, first_unmapped_line + 1))
 
-    def map_cbs_segments(self, cbs_segments):
-        sys.stderr.write("mapping cbs segments...")
+    def map_loci(self, loci):
+        # map a list of loci using the hash table
+        # and report some stats
+        # loci, e.g. tuple(1, 82170)
+
+        sys.stderr.write("mapping loci...")
         unmapped = 0
         first_unmapped = ''
         line_no = 0
 
-        for cbs_seg in cbs_segments:
+        for locus in loci:
             try:
-                self.hash[ (cbs_seg.chr, cbs_seg.start) ]
+                self.chrPos_name_hash[locus]
             except KeyError:
                 unmapped += 1
                 if first_unmapped == '':
-                    first_unmapped = cbs_seg
+                    first_unmapped = locus
                     first_unmapped_line = line_no
         sys.stderr.write("DONE!\n")
 
-        sys.stderr.write("\n%d unmapped segment(s)\n" % unmapped)
+        sys.stderr.write("\n%d unmapped loci\n" % unmapped)
         if first_unmapped != '':
-            sys.stderr.write("1st unmapped segment: %s (line %d) \n" % (first_unmapped, first_unmapped_line))
+            sys.stderr.write("1st unmapped locus: %s (line %d) \n" % (first_unmapped, first_unmapped_line + 1))
 
+    def nearest_two_probes(self, locus):
+        # finds the two probes closest to the locus
+
+        hash = self.get_locus_hash()
+        chr = str(locus[0])
+        pos = str(locus[1])
+
+        loci = hash[chr]
+        loci.sort()     # sorting each time, not sure how expensive this is.
+
+        # these functions are from the pydoc
+        # http://docs.python.org/2.7/library/bisect.html#searching-sorted-lists
+        def find_le(a, x):
+            'Find rightmost value less than or equal to x'
+            i = bisect_right(a, x)
+            if i:
+                return a[i-1]
+            raise ValueError
+        def find_ge(a, x):
+            'Find leftmost item greater than or equal to x'
+            i = bisect_left(a, x)
+            if i != len(a):
+                return a[i]
+            raise ValueError
+
+        le = find_le(loci, pos)
+        ge = find_ge(loci, pos)
+
+        return (le, ge)
+
+    def distance_to_nearest_probe(self, locus):
+        pos = int(locus[1])
+        (l, r) = self.nearest_two_probes(locus)
+        (l, r) = (int(l), int(r))
+
+        return min(abs(l - pos), abs(r - pos))
 
 def map_probes(hash, probes):
     # takes a list of probes and looks for unmapped probes
@@ -212,7 +265,7 @@ def map_probes(hash, probes):
 
     if first_unmapped_probe != '':
         out.write("1st unmapped probe: %s (line %d) \n" \
-                % (first_unmapped_probe, first_unmapped_line))
+                % (first_unmapped_probe, first_unmapped_line + 1))
 
 def map_pos(hash, positions):
     # takes a list of positions and looks for unmapped positions
@@ -297,14 +350,17 @@ def nearest_probes(hash, lookups):
 
 if __name__ == "__main__":
     marker_f = open(sys.argv[1])
-    ms = MarkerPosUtil(marker_f)
+    util = MarkerPosUtil(marker_f)
     marker_f.close()
 
-    ms.make_chr_pos_hash()
-    ms.
+    #marker_signals = read_marker_signals(sys.stdin)
+    #util.map_markers([ms.name for ms in marker_signals])
+
+    #cbs_segs = read_cbs_segments(sys.stdin)
+    #util.map_loci([(seg.chr, seg.start) for seg in cbs_segs])
 
     #ms.map_marker_signals(read_marker_signals(sys.stdin))
-    ms.map_cbs_segments(read_cbs_segments(sys.stdin))
+    #ms.map_cbs_segments(read_cbs_segments(sys.stdin))
 
 ### {{{ arg parser ###
 #parser = argparse.ArgumentParser(description="Utils for dealing with \
