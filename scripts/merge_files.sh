67e8b1f224826ff5ec21aab3379e8bd108dec132
Gideon Dresdner <gideond@gmail.com>
2013-02-01 17:09:05 -0500

    merge files for gistic

--- scripts/merge_files.sh
+++ scripts/merge_files.sh
@@ -2,10 +2,6 @@
 
 # converts seg files into the first format for gistic
 
-if [ ! -d $1 ]; then
-    echo \"$1\" "is not a directory, sorry"
-    exit 1
-fi
-
-
 #tail -q -f -n +2 ls *CBS_out.txt | cut -f1-5,7
+
+tail -q -f -n +2
